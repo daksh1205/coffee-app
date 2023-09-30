a477c93808eb78dcec5e5ae25787f6e5aa4f55a0
daksh <dakshyt1205@gmail.com>
2023-10-01 01:13:48 +0530

    added coffee types tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:coffee_app/util/coffee_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../util/coffee_type.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,6 +12,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of coffee types
+  final List coffeeType = [
+    // { coffee type, isSelected }
+    [
+      'Cappucino',
+      true,
+    ],
+    [
+      'Latte',
+      false,
+    ],
+    [
+      'Black',
+      false,
+    ],
+  ];
+
+  // user tapped on coffee types
+  void coffeeTypeSelected(int index) {
+    setState(() {
+      // this loop makes every selection false
+      for (int i = 0; i < coffeeType.length; i++) {
+        coffeeType[i][1] = false;
+      }
+      coffeeType[index][1] = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,12 +109,42 @@ class _HomePageState extends State<HomePage> {
             height: 25,
           ),
 
+          Container(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: coffeeType.length,
+              itemBuilder: (context, index) {
+                return CoffeeType(
+                    coffeeType: coffeeType[index][0],
+                    isSelected: coffeeType[index][1],
+                    onTap: () {
+                      coffeeTypeSelected(index);
+                    });
+              },
+            ),
+          ),
+
           // horz list view of items
           Expanded(
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                CoffeeTile(),
+                CoffeeTile(
+                  coffeeImagePath: 'lib/images/latte.jpg',
+                  coffeeName: 'Latte',
+                  coffeePrice: '4.20',
+                ),
+                CoffeeTile(
+                  coffeeImagePath: 'lib/images/capp.jpg',
+                  coffeeName: 'Cappucino',
+                  coffeePrice: '4.10',
+                ),
+                CoffeeTile(
+                  coffeeImagePath: 'lib/images/milk.jpg',
+                  coffeeName: 'Milk Coffee thing',
+                  coffeePrice: '4.60',
+                ),
               ],
             ),
           ),
--- lib/util/coffee_tile.dart
+++ lib/util/coffee_tile.dart
@@ -1,17 +1,81 @@
 import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
-  const CoffeeTile({super.key});
+  final String coffeeImagePath;
+  final String coffeeName;
+  final String coffeePrice;
+
+  CoffeeTile({
+    required this.coffeeImagePath,
+    required this.coffeeName,
+    required this.coffeePrice,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0),
       child: Container(
-        width: 200,
+        width: 180,
         decoration: BoxDecoration(
           color: Colors.black54,
           borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 12.0, left: 12.0, right: 12.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  coffeeImagePath,
+                ),
+              ),
+            ),
+            // coffee name
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    coffeeName,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'With Almond Milk',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //price
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('\$' + coffeePrice),
+                  Container(
+                    padding: EdgeInsets.all(4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Icon(Icons.add),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
